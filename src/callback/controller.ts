41a7b1fec96b3380570358456b7d6314206cdbb3
domutala <ibntalla@gmail.com>
2024-06-26 17:11:48 +0100

    default redirect

--- src/callback/controller.ts
+++ src/callback/controller.ts
@@ -12,7 +12,7 @@ export class CallbackController {
     @Res() res: Response,
   ) {
     // https://github.com/login/oauth/authorize?client_id=Iv23liH1peB1fRkwQ8T5&redirect_uri=http://localhost:49000/auth/callback/github&&state=https%3A%2F%2Fdomutala.com
-    // https://github.com/login/oauth/authorize?client_id=Iv23liH1peB1fRkwQ8T5&state=https%3A%2F%2Fdomutala.com
-    return res.redirect(state);
+    // https://github.com/login/oauth/authorize?client_id=Iv23liH1peB1fRkwQ8T5&state=https%3A%2F%2Fgoogle.com
+    return res.redirect(state || "https://domutala.com");
   }
 }
